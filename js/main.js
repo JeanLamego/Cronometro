d37f78b9b26a9601744de5be78a345182a8eb87f
Jean Lamego <jeanlamego@hotmail.com>
2023-07-15 18:27:42 -0300

    função do cronometro saindo

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ const startBtn= document.getElementById("startBtn");
 const pauseBtn= document.getElementById("pauseBtn");
 const resumeBtn= document.getElementById("resetBtn");
 const resetBtn= document.getElementById("resetBtn");
+
+var interval;
+var minutes=0;
+var seconds=0;
+var milliseconds=0;
+var isPaused = false;
+
+startBtn.addEventListener("click", startTimer);
+
+function startTimer(){
+interval = setInterval(() =>{
+    if (!isPaused){
+        milliseconds +=10
+    }
+},10)
+
+}
