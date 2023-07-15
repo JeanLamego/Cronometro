a2253a4b40d544abbd7407fcd52c0bf704b23b17
Jean Lamego <jeanlamego@hotmail.com>
2023-07-15 17:09:10 -0300

    Html pronto, css pronto, iniciando treinamento do JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const minutesEl= document.getElementById("minutes");
+const secondsEl= document.getElementById("seconds");
+const millisecondsEl= document.getElementById("milliseconds");
+const startBtn= document.getElementById("startBtn");
+const pauseBtn= document.getElementById("pauseBtn");
+const resumeBtn= document.getElementById("resetBtn");
+const resetBtn= document.getElementById("resetBtn");
